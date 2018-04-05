9e540d74d6763e27315ca01f8db2fd2bb05314b6
Hendi Santika <hendisantika@gmail.com>
2018-04-05 08:07:05 +0700

    #1 - Initial Project

---
+++ src/main/java/com/hendisantika/redisspringcache/RedisSpringCacheApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.redisspringcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSpringCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisSpringCacheApplication.class, args);
+	}
+}
