ff3bda226f04cd9ef18c01ef56f30ac6f0894e8d
Hendi Santika <hendisantika@gmail.com>
2018-08-21 08:13:19 +0700

    #6 - Upgraded to Spring Boot 1.5.15.RELEASE version

---
+++ src/main/java/com/hendisantika/redisspringcache/MessageProducer.java
@@ -0,0 +1,14 @@
+package com.hendisantika.redisspringcache;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : redis-spring-cache
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/04/18
+ * Time: 08.27
+ * To change this template use File | Settings | File Templates.
+ */
+public class MessageProducer {
+}
